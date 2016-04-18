0ac24a113330d1be0e50f3b7f74727a5d28906e5
Sarah Jung <sjung@ou.edu>
2016-04-17 19:40:36 -0500

    Trying handlebars

--- api.js
+++ api.js
@@ -57,8 +57,8 @@ function select_change(reader){
 function reader_change(reader){
 	$('#grid-basic tbody').remove();
 	$.getJSON("http://head.ouetag.org/api/etag/tag_reads/.json?reader=" + reader + "&ordering=-tag_timestamp&page_size=20", function(data){
-		$.each(data.results, function(key, value){
-			var dataTable = '';
+    var dataTable = '';
+    $.each(data.results, function(key, value){
 			dataTable += value.tag;
 			dataTable += value.tag_timestamp;
 		});
