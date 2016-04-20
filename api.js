14cf7253308a72b9a867f380af6c9d465ce78f31
Sarah Jung <sjung@ou.edu>
2016-04-20 04:58:40 -0500

    Trying handlebars

--- api.js
+++ api.js
@@ -55,15 +55,14 @@ function select_change(reader){
 };
 
 function reader_change(reader){
-	$('#grid-basic tbody').remove();
+	//$('#grid-basic tbody').remove();
 	$.getJSON("http://head.ouetag.org/api/etag/tag_reads/.json?reader=" + reader + "&ordering=-tag_timestamp&page_size=20", function(data){
     var dataTable = [];
     $.each(data.results, function(key, value){
 			dataTable += value.tag;
 			dataTable += value.tag_timestamp;
-      $('#grid-basic').append(dataTable);
 		});
-
+      $('#grid-basic').append(dataTable);
 	})
 };
 
