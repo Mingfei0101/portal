a9c2be0281303a2cd6fb387f8cc53bd91acc6251
Bora Jung <Bora Jung>
2016-10-26 15:26:55 -0500

    Trying to make animal table

--- api.js
+++ api.js
@@ -112,7 +112,7 @@ function lat_long_change(url) {
     });
 };
 
-function tag_change(reader){
+function tag_change(tags){
   $('#tag-container').remove();
   $.getJSON("http://head.ouetag.org/api/etag/tag_animal/.json?tags=" + tags, function(data){
     tags_table_template = Handlebars.templates['tmpl-tags-table'];
