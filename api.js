47ef4c32455a12454963e2430693f35f9061f3d9
Bora Jung <Bora Jung>
2016-10-28 11:57:23 -0500

    Figuring out submit

--- api.js
+++ api.js
@@ -71,12 +71,6 @@ function form_submit(formName){
   return false;
 };
 
-function form_submit_tag(formName){
-  data = $('#'+formName).serializeObject();
-  $.postJSON(data.url, data, "PUT");
-  return false;
-};
-
 function select_change(reader){
 	$.getJSON("http://head.ouetag.org/api/etag/readers/" + reader + "/.json" , function(data){
                 reader_form_template = Handlebars.templates['tmpl-readers-form'];
