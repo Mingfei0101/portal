2c74f2cd85579169b26a68408c2732a04162fba1
Bora Jung <Bora Jung>
2016-10-28 11:55:23 -0500

    Trying to figure out submit

--- api.js
+++ api.js
@@ -77,18 +77,6 @@ function form_submit_tag(formName){
   return false;
 };
 
-function form_submit_modal(formName){
-  data = $('#'+formName).serializeObject();
-  $.postJSON(data.url, data, "PUT");
-  return false;
-};
-
-function form_submit_modal_tag(formName){
-  data = $('#'+formName).serializeObject();
-  $.postJSON(data.url, data, "PUT");
-  return false;
-};
-
 function select_change(reader){
 	$.getJSON("http://head.ouetag.org/api/etag/readers/" + reader + "/.json" , function(data){
                 reader_form_template = Handlebars.templates['tmpl-readers-form'];
