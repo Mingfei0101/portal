85fb289e3d5313c7763857a6f9fef8534e08a2f8
Bora Jung <Bora Jung>
2016-11-04 10:19:45 -0500

    Trying to have the popup submit their forms

--- api.js
+++ api.js
@@ -71,6 +71,12 @@ function form_submit(formName){
   return false;
 };
 
+function form_submit2(formName){
+  data = $('#'+formName).serializeObject();
+  $.postJSON(data.url.url, data, "PUT");
+  return false;
+};
+
 function select_change(reader){
 	$.getJSON("http://head.ouetag.org/api/etag/readers/" + reader + "/.json" , function(data){
                 reader_form_template = Handlebars.templates['tmpl-readers-form'];
